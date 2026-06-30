a3c81ec108b84dbcccac5c29202c1a4b742c5028
utkarsa <pillautkarsa@gmail.com>
2026-06-30 13:51:35 +0530

    chore: initialize Spring Boot project structure

---
+++ src/main/java/com/utkarsa/notes/NotesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.utkarsa.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utkarsa/notes/NotesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsa.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

37926660278aa6d1617327b2325450d7b3487069
utkarsa <pillautkarsa@gmail.com>
2026-06-30 17:34:53 +0530

    feat: create Note entity, repository and service

--- src/main/java/com/utkarsa/notes/controller/NoteController.java
+++ src/main/java/com/utkarsa/notes/controller/NoteController.java
@@ -2,6 +2,7 @@ package com.utkarsa.notes.controller;
 
 import com.utkarsa.notes.service.NoteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class NoteController {
     private final NoteService noteService;
+
+    @PostMapping
+    public void createNote() {
+        noteService.createNote();
+    }
 }

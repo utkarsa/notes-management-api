b768c3909d8f5fb935183b8e01dea4afde061f12
utkarsa <pillautkarsa@gmail.com>
2026-06-30 15:27:40 +0530

    feat: create Note entity

--- src/main/java/com/utkarsa/notes/entity/Note.java
+++ src/main/java/com/utkarsa/notes/entity/Note.java
@@ -1,7 +1,28 @@
 package com.utkarsa.notes.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private boolean isPinned;
+    private boolean isDeleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
